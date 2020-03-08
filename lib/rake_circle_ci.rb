063f9fc060147d37d6ddc8dec309261044722671
Toby Clemson <tobyclemson@gmail.com>
2020-03-08 01:05:50 +0000

    Add define helper for environment variable tasks.

--- lib/rake_circle_ci.rb
+++ lib/rake_circle_ci.rb
@@ -4,5 +4,7 @@ require 'rake_circle_ci/tasks'
 require 'rake_circle_ci/task_sets'
 
 module RakeCircleCI
-
+  def self.define_environment_variables_tasks(opts = {}, &block)
+    RakeCircleCI::TaskSets::EnvironmentVariables.define(opts, &block)
+  end
 end
--- spec/rake_circle_ci_spec.rb
+++ spec/rake_circle_ci_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe RakeCircleCI do
   it 'has a version number' do
     expect(RakeCircleCI::VERSION).not_to be nil
   end
+
+  context 'define_environment_variables_tasks' do
+    context 'when instantiating RakeCircleCI::TaskSets::EnvironmentVariables' do
+      it 'passes the provided block' do
+        opts = {
+            project_slug: 'github/org/repo'
+        }
+
+        block = lambda do |t|
+          t.api_token = 'some-token'
+          t.environment_variables = {
+              THING_ONE: 'value-1',
+              THING_TWO: 'value-2',
+          }
+        end
+
+        expect(RakeCircleCI::TaskSets::EnvironmentVariables)
+            .to(receive(:define) do |passed_opts, &passed_block|
+              expect(passed_opts).to(eq(opts))
+              expect(passed_block).to(eq(block))
+            end)
+
+        RakeCircleCI.define_environment_variables_tasks(opts, &block)
+      end
+    end
+  end
 end
